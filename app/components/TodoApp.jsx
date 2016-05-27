bae7759b69936404717c6c41445adb602d1592d3
Jonathan Brown <jonathankenbrown@gmail.com>
2016-05-26 18:00:39 -0700

    Add TodoForm to add Todo and Test

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,5 +1,6 @@
 var React = require('react');
 var TodoList = require('TodoList');  // #3
+var TodoForm = require('TodoForm');
 
 var TodoApp = React.createClass({
   getInitialState: function () {        // #1
@@ -21,11 +22,16 @@ var TodoApp = React.createClass({
       ]
     };
   },
+  handleAddTodo: function (text) {
+    alert('new todo: ' + text);
+
+  },
   render: function () {
     var {todos} = this.state;         // #2 , #4 to add TodoList
     return (
       <div>
         <TodoList todos={todos}/>
+        <TodoForm onAddTodo={this.handleAddTodo}/>
       </div>
     );
   }
