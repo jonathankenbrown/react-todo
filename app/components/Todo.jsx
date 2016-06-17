089569d9fc120c8d123682e260b7c838cb1fa139
Jonathan Brown <jonathankenbrown@gmail.com>
2016-06-17 10:27:31 -0700

    Adding TodoModal - still construction

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -3,6 +3,8 @@ var {connect} = require('react-redux');
 var moment = require('moment');
 var actions = require('actions');
 
+var TodoModal = require('TodoModal');
+
 export var Todo = React.createClass({
   render: function () {
     var {id, text, completed, createdAt, completedAt, dispatch} = this.props;
@@ -20,15 +22,22 @@ export var Todo = React.createClass({
     };
 
     return (
-      <div className={todoClassName} onClick={() => {
-          dispatch(actions.startToggleTodo(id, !completed));
-        }}>
-        <div>
-          <input type="checkbox" checked={completed}/>
+      <div>
+        <div className={todoClassName} onClick={() => {
+            dispatch(actions.startToggleTodo(id, !completed));
+          }}>
+          <div>
+            <input type="checkbox" checked={completed}/>
+          </div>
+          <div>
+            <p>{text}</p>
+            <p className="todo__subtext">{renderDate()}</p>
+          </div>
         </div>
         <div>
-          <p>{text}</p>
-          <p className="todo__subtext">{renderDate()}</p>
+          <a href="#" onClick={TodoModal}>
+            TestModalLink
+          </a>
         </div>
       </div>
     )
---
+++ app/components/TodoModal.jsx
@@ -0,0 +1,45 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var ReactDOMServer = require('react-dom/server');
+
+var TodoModal = React.createClass({
+  getDefaultProps: function () {
+    return {
+      title: 'Error'
+    };
+  },
+  // propTypes: {
+  //   title: React.PropTypes.string,
+  //   message: React.PropTypes.string.isRequired
+  // },
+  componentDidMount: function () {
+    // var {title, message} = this.props;
+    var modalMarkup = (
+      <div id="error-modal" className="reveal tiny text-center" data-reveal="">
+        <h4>title</h4>
+        <p>test</p>
+        <p>
+          <button className="button hollow" data-close="">
+            Okay
+          </button>
+        </p>
+      </div>
+    );
+
+    var $modal = $(ReactDOMServer.renderToString(modalMarkup));
+    $(ReactDOM.findDOMNode(this)).html($modal);
+
+    var modal = new Foundation.Reveal($('#error-modal'));
+    modal.open();
+  },
+  render: function () {  // had to move them out to reactDOM related function (inside componentDidMount) as state was being changed and React didn't like it
+
+
+    return (
+      <div>
+      </div>
+    );
+  }
+});
+
+module.exports = TodoModal;
